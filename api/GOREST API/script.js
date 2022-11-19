ed4dca6a3c863eb7d04f791d067f7b5f14d14ee6
fransbachtiar24 <fransbachtiar5@mhs.mdp.ac.id>
2022-11-19 08:02:41 +0700

    susu beruang

--- api/GOREST API/script.js
+++ api/GOREST API/script.js
@@ -1,42 +1,44 @@
-let userList = document.getElementById("listUser");
+let listUser = document.getElementById("listUser")
 
-fetch("https://gorest.co.in/public/v2/users").then(function (response) {
-  if (response.status != 200) {
-    console.log("Oopss Server Tidak Bisa Di Akses");
-    return;
-  }
-  response.json().then(function (data) {
-    console.log(data);
-    data.forEach(getAllUser);
-  });
-});
+getUser()
 
-// handle delete
-const handleDelete = (id) => {
-  console.log("hapus data id" + id);
-  fetch("https://gorest.co.in/public/v2/users" + id, {
-    method: "DELETE",
-    headers: {
-      Autorization:
-        "Bearer 4f132e6355c55833d01e0dbebc067e6479b208bb743cb35b16baeee044c6358d",
-    },
-  }).then((response) => {
-    console.log(response);
-    userList.innerHTML = "";
-    getAllUser();
-  });
-};
+function getUser() {
+    fetch("https://gorest.co.in/public/v2/users")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            data.forEach(showUser)
+        })
+        .catch(error => {
+            console.log(error)
+        });
+}
 
-const getAllUser = (item, index) => {
-  userList.innerHTML += `
-    <tr>
-    <td>${item.email}</td>
-    <td>${item.name}</td>
-    <td>${item.gender}</td>
-    <td>${item.status}</td>
-    <td>
-    <button class="btn btn-warning mx-2">Edit</>
-    <button class="btn btn-danger mx-2" onClik="deleteuser(${item.id})">Delete</>
-    </td>
-    </tr>`;
-};
+function showUser(value, index) {
+    listUser.innerHTML += `<tr>
+    <td>${value.email}</td>
+    <td>${value.name}</td>
+    <td>${value.gender}</td>
+    <td>${value.status}</td>
+    <td><button class="btn btn-info" onclick="editUser(${value.id})">Edit</button>
+    <button class="btn btn-danger" onclick="deleteUser(${value.id})">Delete</button></td>
+    </tr>`
+}
+
+function deleteUser(id) {
+    console.log("Hapus data id: " + id)
+    fetch("https://gorest.co.in/public/v2/users/" + id, {
+            method: "DELETE",
+            headers: {
+                Authorization: "Bearer 51530658d1529e61cbe231c3fd226b95cc69da691cad7075f0ead6408f656508"
+            }
+        })
+        .then(response => {
+            console.log(response)
+            listUser.innerHTML = "" // kosongkan tabel list user
+            getUser() // panggil function getUser()
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
